0a8165331ade0415d23489009f5e3ae1e8ebb1ea
yellow <yellow@battosai.yellow.dojo>
2021-08-18 19:45:55 -0400

    Primeira Aula Python

---
+++ whois_001.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import whois
+target="google.com"
+querywhois = whois.query(target)
+print(querywhois.name)
+print(querywhois.creation_date)
+print(querywhois.expiration_date)
+print(querywhois.registrar)
+print(querywhois.last_updated)
+print(querywhois.name_servers)
